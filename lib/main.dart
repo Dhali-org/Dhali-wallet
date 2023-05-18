aec328d670b35a4affffaa17c56279f6abd585d2
David Simmons <dr.desimmons@gmail.com>
2023-05-18 19:00:14 +0100

    Add main.dart and web

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:dhali_wallet/dhali_wallet.dart';
+import 'package:dhali_wallet/xrpl_wallet.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  XRPLWallet? _wallet;
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+          body: WalletHomeScreen(
+        color: Color(0xFF0000FF),
+        textColor: Colors.white,
+        highlightedColor: Color(0xFF0000FF),
+        title: "wallet",
+        getWallet: () {
+          return _wallet;
+        },
+        setWallet: (XRPLWallet wallet) {
+          _wallet = wallet;
+        },
+      )),
+    ),
+  );
+}
