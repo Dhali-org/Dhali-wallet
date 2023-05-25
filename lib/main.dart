6bf4507d15e0194027ab51dffa317e337f010edc
rjdaniels <danielsrj@protonmail.com>
2023-05-25 14:11:41 +0100

    add 'onActivation' callback

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,59 @@ import 'package:dhali_wallet/dhali_wallet_widget.dart';
 import 'package:dhali_wallet/xrpl_wallet.dart';
 import 'package:flutter/material.dart';
 
-Future<void> main() async {
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   DhaliWallet? _wallet;
-  runApp(
-    MaterialApp(
+  bool _activated = false;
+  Key _key = UniqueKey();
+
+  void _activateWallet() {
+    setState(() {
+      _activated = true;
+    });
+  }
+
+  void _removeFloatingActionButton() {
+    setState(() {
+      _key = UniqueKey();
+      _activated = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       theme: ThemeData.dark(),
-      home: WalletHomeScreen(
-        isImported: false,
-        bodyTextColor: Color.fromARGB(255, 255, 255, 255),
-        buttonsColor: Color.fromARGB(255, 255, 0, 212),
-        title: "wallet",
-        getWallet: () {
-          return _wallet;
-        },
-        setWallet: (DhaliWallet wallet) {
-          _wallet = wallet;
-        },
+      home: Scaffold(
+        body: WalletHomeScreen(
+          isImported: false,
+          bodyTextColor: Color.fromARGB(255, 255, 255, 255),
+          buttonsColor: Color.fromARGB(255, 255, 0, 212),
+          title: "wallet",
+          getWallet: () {
+            return _wallet;
+          },
+          setWallet: (DhaliWallet wallet) {
+            _wallet = wallet;
+          },
+          onActivation: _activateWallet,
+        ),
+        floatingActionButton: _activated
+            ? FloatingActionButton.extended(
+                label: Text("Press me to make me disappear!"),
+                onPressed: _removeFloatingActionButton,
+              )
+            : null,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
-    ),
-  );
+    );
+  }
+}
+
+Future<void> main() async {
+  runApp(MainApp());
 }
