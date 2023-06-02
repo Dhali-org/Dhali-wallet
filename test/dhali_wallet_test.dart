4ae31134a618e08d491cc96396ff8bc411c017cc
David Simmons <dr.desimmons@gmail.com>
2023-06-02 11:54:00 +0100

    Fix test

--- test/dhali_wallet_test.dart
+++ test/dhali_wallet_test.dart
@@ -100,9 +100,6 @@ void main() {
       expect(find.text("Please select a wallet from 'Available wallets'"),
           findsNothing);
 
-      await tester.tap(find.byKey(const Key("raw_xrp_wallet_tile")));
-      await tester.pumpAndSettle();
-
       expect(find.text('Balance: 1 XRP'), findsOneWidget);
       expect(find.text('Classic address: a-random-address'), findsOneWidget);
       expect(find.text('Memorable words: '), findsOneWidget);
