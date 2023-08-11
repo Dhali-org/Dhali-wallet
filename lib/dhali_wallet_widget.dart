3e41a7784b97f13297e6020209cbcfa2c5dd1faf
David Simmons <dr.desimmons@gmail.com>
2023-08-11 18:54:11 +0100

    Remove wallet button border

--- lib/dhali_wallet_widget.dart
+++ lib/dhali_wallet_widget.dart
@@ -450,8 +450,7 @@ class _WalletHomeScreenState extends State<WalletHomeScreen>
                           MaterialStateProperty.all(widget.buttonsColor),
                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                           RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              side: BorderSide()))),
+                              borderRadius: BorderRadius.circular(18.0)))),
                   onPressed: () {
                     setState(() {
                       _wallet = wallet;
