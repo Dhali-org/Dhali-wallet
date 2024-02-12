15749c9a6aa1c9040e3df648cead10892d9eb80a
David Simmons <dr.desimmons@gmail.com>
2024-02-12 16:08:34 +0000

    Revert "Only display test walet on integration test"
    This reverts commit 0dfe17097153f3f0807d2608b3d5289f9c40d15a.

--- lib/dhali_wallet_widget.dart
+++ lib/dhali_wallet_widget.dart
@@ -350,8 +350,7 @@ class _WalletHomeScreenState extends State<WalletHomeScreen>
                     height: icon_height);
                 text = " Link XUMM wallet";
                 spacer = SizedBox(height: spacer_height);
-              } else if (wallet == Wallet.RawXRPWallet &&
-                  const String.fromEnvironment('INTEGRATION') != "true") {
+              } else if (wallet == Wallet.RawXRPWallet) {
                 key = Key("raw_xrp_wallet_tile");
                 image = Image.asset(
                   widget.isImported
